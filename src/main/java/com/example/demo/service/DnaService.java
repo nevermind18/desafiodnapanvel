d04091c4b76d0f40d8fd8098879fee9638c90733
bruno. fontoura <brunofontoura805@gmail.com>
2024-10-11 23:38:33 -0300

    ajustes no codigo

--- src/main/java/com/example/demo/service/DnaService.java
+++ src/main/java/com/example/demo/service/DnaService.java
@@ -19,15 +19,64 @@ public class DnaService {
 
         for (int i = 0; i < tamanho; i++) {
 
-            if (verificaSequencia(array[i])) {
+            if (verificaSequencia(array[i]) || verificaColuna(array, i)) {
                 dna.setMutante(true);
             }
+        }
+
+        if (verificaDiagonais(array)) {
+            dna.setMutante(true);
         }
 
         dnaRepository.save(dna);
         return dna;
     }
 
+    private boolean verificaColuna(String[] array, int coluna) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < array.length; i++) {
+            sb.append(array[i].charAt(coluna));
+        }
+        return verificaSequencia(sb.toString());
+    }
+
+    private boolean verificaDiagonais(String[] array) {
+        int n = array.length;
+
+        for (int i = 0; i < n; i++) {
+            if (verificaDiagonalPrincipal(array, 0, i) || verificaDiagonalSecundaria(array, 0, n - 1 - i)) {
+                return true;
+            }
+        }
+
+        for (int i = 1; i < n; i++) {
+            if (verificaDiagonalPrincipal(array, i, 0) || verificaDiagonalSecundaria(array, i, n - 1)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean verificaDiagonalPrincipal(String[] array, int linha, int coluna) {
+        StringBuilder sb = new StringBuilder();
+        while (linha < array.length && coluna < array.length) {
+            sb.append(array[linha].charAt(coluna));
+            linha++;
+            coluna++;
+        }
+        return verificaSequencia(sb.toString());
+    }
+
+    private boolean verificaDiagonalSecundaria(String[] array, int linha, int coluna) {
+        StringBuilder sb = new StringBuilder();
+        while (linha < array.length && coluna >= 0) {
+            sb.append(array[linha].charAt(coluna));
+            linha++;
+            coluna--;
+        }
+        return verificaSequencia(sb.toString());
+    }
 
     private boolean verificaSequencia(String sequencia) {
         int contador = 1;
